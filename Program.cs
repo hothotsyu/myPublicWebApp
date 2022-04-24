f4905c9d3a0b5298fb19f17f57e6a038d967fa6a
Peter Woodland <hothotsyu@hotmail.com>
2022-04-24 21:02:54 +0900

    Feature 1 added to Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+// Changed the Source firstly!
 namespace myPublicWebApp
 {
     public class Program
